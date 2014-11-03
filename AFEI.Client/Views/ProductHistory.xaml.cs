17425257f34facc6bfb4686d9e091c35c7784ff7
Stephania Cavadas <stephaniacc93@gmail.com>
2014-11-02 18:15:54 -0800

    Modificaciones UI

--- AFEI.Client/Views/ProductHistory.xaml.cs
+++ AFEI.Client/Views/ProductHistory.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AFEI.Client.Font;
 using MahApps.Metro.Controls;
 
 namespace AFEI.Client.Views
@@ -23,6 +24,11 @@ namespace AFEI.Client.Views
         public ProductHistory()
         {
             InitializeComponent();
+        }
+
+        private void ExportToExcelButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            ExportToExcel.Export(ProductHistoryDataGrid);
         }
     }
 }
